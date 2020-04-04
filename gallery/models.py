cd0a03cec680c01834768aa19235e1c3181b6efc
zapanet1001 <ipolo1969@ufl.edu>
2020-04-04 14:45:49 -0400

    Initial Commit

---
+++ gallery/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1024)
+    
+    def __str__(self):
+        return self.title
+
+class Photograph(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos', default = 'photos/1200px-No_image_available.svg.png')
+    description = models.TextField(max_length=1024)
+    creation_date = models.DateField('date created')
+
+    def __str__(self):
+        return self.name
