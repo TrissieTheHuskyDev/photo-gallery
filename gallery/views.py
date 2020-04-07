2123546380118ba34f62ab0139355bc648103903
zapanet1001 <ipolo1969@ufl.edu>
2020-04-07 10:09:53 -0400

    Configured Photo Detail Page

--- gallery/views.py
+++ gallery/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
 
-from .models import Album
+from .models import Album,Photograph
 
 def index(request):
     album_list = Album.objects.order_by('-title')[:5]
@@ -14,5 +14,5 @@ def album_detail(request, album_id):
     return render(request, 'gallery/album_detail.html', {'album': album})
 
 def photo_detail(request, album_id, photo_id):
-    response = "You're looking at photo %s of album %s."
-    return HttpResponse(response % (photo_id, album_id))
+    photo = get_object_or_404(Photograph, pk = photo_id)
+    return render(request, 'gallery/photo_detail.html', {'photo': photo})
