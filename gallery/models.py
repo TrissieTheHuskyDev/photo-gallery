1316b80d9b1f4058b4b8dd2cc7768675ae123009
zapanet1001 <ipolo1969@ufl.edu>
2020-04-06 22:52:24 -0400

    Updated Homepage CSS

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Album(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=1024)
+    thumbnail = models.ImageField(upload_to='photos/', default = 'media/photos/no-image.png')
     
     def __str__(self):
         return self.title
@@ -12,7 +13,7 @@ class Album(models.Model):
 class Photograph(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='photos/', default = 'photos/1200px-No_image_available.svg.png')
+    photo = models.ImageField(upload_to='photos/', default = 'photos/no-image.png')
     description = models.TextField(max_length=1024)
     creation_date = models.DateField('date created')
 
