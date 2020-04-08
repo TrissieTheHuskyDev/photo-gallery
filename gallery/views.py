6b0e7cba30d243da5ada667a72a19d3edf3bc6ca
zapanet1001 <ipolo1969@ufl.edu>
2020-04-08 12:05:56 -0400

    Fixed Carousel Order

--- gallery/views.py
+++ gallery/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.db.models import Q
 # Create your views here.
 
 from .models import Album,Photograph
 
 def index(request):
-    album_list = Album.objects.order_by('-title')[:5]
-    context = {'album_list': album_list}
-    return render(request, 'gallery/index.html', context)
+    album_list = Album.objects.order_by('-title')
+    return render(request, 'gallery/index.html', {'album_list': album_list})
 
 def album_detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
@@ -16,8 +16,17 @@ def album_detail(request, album_id):
 def photo_detail(request, album_id, photo_id):
     start_photo = get_object_or_404(Photograph, pk = photo_id)
     album = get_object_or_404(Album, pk=album_id)
-    photo_list = album.photograph_set.all()
-    return render(request, 'gallery/photo_detail.html', {'start_photo': start_photo, 'album':album, 'photo_list':photo_list})
+    photo_list = album.photograph_set.exclude(pk=photo_id)
+    prev_photos = album.photograph_set.filter(pk__lt=photo_id)
+    next_photos = album.photograph_set.filter(pk__gt=photo_id)
+    # return render(request, 'gallery/photo_detail.html', {'start_photo': start_photo, 'album':album, 'photo_list':photo_list})
+    return render(request, 'gallery/photo_detail.html', 
+    {   'start_photo': start_photo, 
+        'album':album, 
+        'photo_list':photo_list,
+        'prev_photos': prev_photos,
+        'next_photos': next_photos
+    })
 
 def about(request):
     return render(request, 'gallery/about.html')
