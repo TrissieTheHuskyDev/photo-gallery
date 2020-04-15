ea1fac04d0cd8854a4abab7243b6eb6cd9fa1414
zapanet1001 <ipolo1969@ufl.edu>
2020-04-15 15:38:18 -0400

    Connected ListAPIView for albums with route

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from . import views
+from . import views, api_views
 
 app_name = 'gallery'
 urlpatterns = [
+    path('api/v1/albums', api_views.AlbumList.as_view()),
+
     # '/gallery/
     path('', views.index, name='index'),
     # '/gallery/1/
@@ -12,4 +14,5 @@ urlpatterns = [
     path('<int:album_id>/<int:photo_id>/', views.photo_detail, name='photo_detail'),
     # '/gallery/about
     path('about', views.about, name='about'),
+
 ]
