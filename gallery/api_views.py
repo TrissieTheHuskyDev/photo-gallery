c449b18995f2e3478938c44c3fe322bb672dce06
zapanet1001 <ipolo1969@ufl.edu>
2020-04-15 17:42:28 -0400

    Enabled pagination of querysets in API

--- gallery/api_views.py
+++ gallery/api_views.py
@@ -1,9 +1,14 @@
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 from gallery.serializers import AlbumSerializer
 from gallery.models import Album
+
+class AlbumsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
 class AlbumList(ListAPIView):
     queryset = Album.objects.all()
@@ -11,3 +16,4 @@ class AlbumList(ListAPIView):
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
     search_fields = ('title', 'description')
+    pagination_class = AlbumsPagination
