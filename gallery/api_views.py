c0ff956d1324ebdd7259f16207e1277145bd88f9
zapanet1001 <ipolo1969@ufl.edu>
2020-04-15 16:44:00 -0400

    Added DjangoFilterBackend and SearchFilter to AlbumList

--- gallery/api_views.py
+++ gallery/api_views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 from gallery.serializers import AlbumSerializer
 from gallery.models import Album
@@ -6,3 +8,6 @@ from gallery.models import Album
 class AlbumList(ListAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filter_fields = ('id',)
+    search_fields = ('title', 'description')
