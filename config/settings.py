6b87e7ef10731012790f75947b789e428a48d827
zapanet1001 <ipolo1969@ufl.edu>
2020-05-08 00:02:43 -0400

    Template extension for html pages

--- config/settings.py
+++ config/settings.py
@@ -13,12 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import environ
 
-
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env(DEBUG=(bool, False))
 
